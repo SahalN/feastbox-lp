1c5fddac6d336d41da383e0c627693f095325860
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2024-02-07 23:56:32 +0700

    FINISH PRODUCTION MODE V1

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,12 +7,12 @@ import "./global.css";
 export default function RootLayout({ children }) {
   return (
     <html lang='en' className={oi.variable}>
-      <body className='flex flex-col min-h-screen text-white '>
+      <body className='flex flex-col min-h-screen text-white w-full bg-[#FFA200]'>
         <header className=' bg-gradient-to-b from-[#FF7B00] to-black px-4 py-8'>
           <Header />
         </header>
-        <main className='flex-grow bg-[#FFA200]'>{children}</main>
-        <footer className='px-4 py-8 bg-[#FFA200] text-black '>
+        <main className='flex-grow '>{children}</main>
+        <footer className='px-4 py-8 text-black '>
           <Footer />
         </footer>
       </body>
--- app/page.jsx
+++ app/page.jsx
@@ -33,31 +33,48 @@ export default function LandingPage() {
         </div>
       </div>
       {/*  */}
-      <div className='px-4 py-8 bg-[#FFA200]'>
-        <div>1</div>
-        <div>2</div>
+      <div className='px-4 py-8 font-normal text-black'>
         <div>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
+          <h1 className='font-oi'>Why Choose FeastBox</h1>
+        </div>
+        <div>
+          <img src='' alt='' />
+        </div>
+        <div className='flex flex-col gap-4 py-4'>
+          <div>
+            <h3>Global Cuisine</h3>
+            <p>Bring the world to your kitchen with our exotic menu</p>
+          </div>
+          <div>
+            <h3>Effortless Ordering</h3>
+            <p>A few clicks and get ready to feast!</p>
+          </div>
+          <div>
+            <h3>Quality Ingredients</h3>
+            <p>Fresh and locally sourced produce just for you</p>
+          </div>
         </div>
       </div>
       {/*  */}
-      <div className='px-4 py-8 bg-[#FFA200] text-black'>
+      <div className='px-4 py-8 text-black'>
         <div>
-          <h3>Get Started</h3>
+          <h3 className='flex justify-center pb-4 text-2xl font-oi'>
+            Get Started
+          </h3>
           <p>
             Are you ready to become a global home-chef? Say goodbye to mundane
             meals and hello to an epic culinary journey. Click below to start
             the feast!
           </p>
         </div>
-        <div>
-          <button></button>
+        <div className='py-4'>
+          <button className='focus:outline-none text-black  bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium  rounded-lg text-sm px-5 py-2.5 me-2 mb-2 w-full'>
+            Order Now
+          </button>
         </div>
       </div>
       {/*  */}
-      <div className='flex flex-col gap-4 py-8 text-black bg-[#FFA200] px-4 '>
+      <div className='flex flex-col gap-4 px-4 py-8 text-black '>
         <div className='font-oi'>Got Questions? We’ve Got Answers!</div>
         <div>
           <h3>How does the menu selection work?</h3>
